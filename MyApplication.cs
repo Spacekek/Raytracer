4333013817f52bf86ff34a6c8bbe8c2bf1cf89a5
Tsjerk Piter Walinga <t.p.walinga@hotmail.com>
2024-05-02 14:08:40 +0200

    removed unused "using" statements

--- MyApplication.cs
+++ MyApplication.cs
@@ -1,9 +1,3 @@
-using System;
-using System.Collections.Generic;
-using OpenTK;
-using OpenTK.Graphics;
-using OpenTK.Graphics.OpenGL;
-using OpenTK.Input;
 using OpenTK.Mathematics;
 
 namespace Template
